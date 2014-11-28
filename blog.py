d32b74a8d3963b171df3c5684a0ca7c06c1b2f90
Lu Lihao <lulihao@papayamobile.com>
2014-11-28 17:22:55 +0800

    update blog

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2009 Bret Taylor
+# Copyright 2009 Facebook
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -15,14 +15,12 @@
 # under the License.
 
 import functools
-import markdown
-import os
 import os.path
 import re
+import tornado.escape
 import tornado.web
 import tornado.wsgi
 import unicodedata
-import wsgiref.handlers
 
 from google.appengine.api import users
 from google.appengine.ext import db
@@ -33,8 +31,8 @@ class Entry(db.Model):
     author = db.UserProperty()
     title = db.StringProperty(required=True)
     slug = db.StringProperty(required=True)
-    body = db.TextProperty(required=True)
-    markdown = db.TextProperty(required=True)
+    body_source = db.TextProperty(required=True)
+    html = db.TextProperty(required=True)
     published = db.DateTimeProperty(auto_now_add=True)
     updated = db.DateTimeProperty(auto_now=True)
 
@@ -47,12 +45,12 @@ def administrator(method):
             if self.request.method == "GET":
                 self.redirect(self.get_login_url())
                 return
-            raise web.HTTPError(403)
+            raise tornado.web.HTTPError(403)
         elif not self.current_user.administrator:
             if self.request.method == "GET":
                 self.redirect("/")
                 return
-            raise web.HTTPError(403)
+            raise tornado.web.HTTPError(403)
         else:
             return method(self, *args, **kwargs)
     return wrapper
@@ -68,15 +66,16 @@ class BaseHandler(tornado.web.RequestHandler):
     def get_login_url(self):
         return users.create_login_url(self.request.uri)
 
-    def render_string(self, template_name, **kwargs):
+    def get_template_namespace(self):
         # Let the templates access the users module to generate login URLs
-        return tornado.web.RequestHandler.render_string(
-            self, template_name, users=users, **kwargs)
+        ns = super(BaseHandler, self).get_template_namespace()
+        ns['users'] = users
+        return ns
 
 
 class HomeHandler(BaseHandler):
     def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=3)
+        entries = db.Query(Entry).order('-published').fetch(limit=5)
         if not entries:
             if not self.current_user or self.current_user.administrator:
                 self.redirect("/compose")
@@ -85,7 +84,6 @@ class HomeHandler(BaseHandler):
 
 
 class EntryHandler(BaseHandler):
-    @tornado.web.removeslash
     def get(self, slug):
         entry = db.Query(Entry).filter("slug =", slug).get()
         if not entry: raise tornado.web.HTTPError(404)
@@ -96,11 +94,6 @@ class ArchiveHandler(BaseHandler):
     def get(self):
         entries = db.Query(Entry).order('-published')
         self.render("archive.html", entries=entries)
-
-
-class AboutHandler(BaseHandler):
-    def get(self):
-        self.render("about.html")
 
 
 class FeedHandler(BaseHandler):
@@ -123,8 +116,9 @@ class ComposeHandler(BaseHandler):
         if key:
             entry = Entry.get(key)
             entry.title = self.get_argument("title")
-            entry.body = self.get_argument("markdown")
-            entry.markdown = markdown.markdown(self.get_argument("markdown"))
+            entry.body_source = self.get_argument("body_source")
+            entry.html = tornado.escape.linkify(
+                self.get_argument("body_source"))
         else:
             title = self.get_argument("title")
             slug = unicodedata.normalize("NFKD", title).encode(
@@ -141,54 +135,30 @@ class ComposeHandler(BaseHandler):
                 author=self.current_user,
                 title=title,
                 slug=slug,
-                body=self.get_argument("markdown"),
-                markdown=markdown.markdown(self.get_argument("markdown")),
+                body_source=self.get_argument("body_source"),
+                html=tornado.escape.linkify(self.get_argument("body_source")),
             )
         entry.put()
         self.redirect("/entry/" + entry.slug)
 
 
 class EntryModule(tornado.web.UIModule):
-    def render(self, entry, show_comments=False):
-        if show_comments:
-            self.show_comments = True
-        else:
-            self.show_count = True
-        return self.render_string("modules/entry.html", entry=entry,
-                                  show_comments=show_comments)
-
-    def embedded_javascript(self):
-        if getattr(self, "show_count", False):
-            return self.render_string("disquscount.js")
-        return None
-
-    def javascript_files(self):
-        if getattr(self, "show_comments", False):
-            return ["http://disqus.com/forums/brettaylor/embed.js"]
-        return None
+    def render(self, entry):
+        return self.render_string("modules/entry.html", entry=entry)
 
 
 settings = {
-    "blog_title": u"Peter Lu's blog",
+    "blog_title": u"Tornado Blog",
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
     "ui_modules": {"Entry": EntryModule},
     "xsrf_cookies": True,
-    "debug": os.environ.get("SERVER_SOFTWARE", "").startswith("Development/"),
 }
-application = tornado.wsgi.WSGIApplication([
+application = tornado.web.Application([
     (r"/", HomeHandler),
     (r"/archive", ArchiveHandler),
     (r"/feed", FeedHandler),
-    (r"/entry/([^/]+)/?", EntryHandler),
+    (r"/entry/([^/]+)", EntryHandler),
     (r"/compose", ComposeHandler),
-    (r"/about", AboutHandler),
-    (r"/index", tornado.web.RedirectHandler, {"url": "/archive"}),
 ], **settings)
 
-
-def main():
-    wsgiref.handlers.CGIHandler().run(application)
-
-
-if __name__ == "__main__":
-    main()
+application = tornado.wsgi.WSGIAdapter(application)
